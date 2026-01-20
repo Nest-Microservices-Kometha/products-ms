a3ec9f085777a995fe0ab341dc2ba3e67c8492e2
Keneth <kenethcubas2002@unitec.edu>
2026-01-19 19:15:55 -0600

    feat: implement products microservice with CRUD operations
    - Add main entry point for microservice in `src/main.ts`
    - Create DTOs for product creation and updating in `src/products/dto/`
    - Define product entity in `src/products/entities/product.entity.ts`
    - Implement products controller with message patterns for microservice in `src/products/products.controller.ts`
    - Create products module to encapsulate service and controller in `src/products/products.module.ts`
    - Develop products service with database connection and CRUD logic in `src/products/products.service.ts`
    - Add end-to-end tests for application in `test/app.e2e-spec.ts`
    - Configure Jest for end-to-end testing in `test/jest-e2e.json`
    - Set up TypeScript configuration for building in `tsconfig.build.json`
    - Establish base TypeScript configuration in `tsconfig.json`

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,14 @@
+import { IsNumber, Min, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateProductDto {
+  @IsString()
+  public name: string;
+
+  @IsNumber({
+    maxDecimalPlaces: 4,
+  })
+  @Min(0)
+  @Type(() => Number)
+  public price: number;
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,106 @@
+import {
+  Injectable,
+  Logger,
+  NotFoundException,
+  OnModuleInit,
+} from '@nestjs/common';
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { envs } from '../config';
+import { PaginationDto } from 'src/common';
+import { Prisma, PrismaClient } from 'src/generated/prisma/client';
+
+@Injectable()
+export class ProductsService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger(ProductsService.name);
+
+  constructor() {
+    const adapter = new PrismaBetterSqlite3({ url: envs.databaseUrl });
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Connected to the database');
+  }
+
+  create(createProductDto: CreateProductDto) {
+    return this.product.create({ data: createProductDto });
+  }
+
+  async findAll(paginationDto: PaginationDto) {
+    const { page, limit } = paginationDto;
+
+    if (!page || !limit) {
+      return this.product.findMany();
+    }
+
+    const totalPage = await this.product.count({where: { available: true}});
+
+    return {
+      data: await this.product.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+        where: {
+          available: true
+        }
+      }),
+      meta: {
+        totalItems: totalPage,
+        itemsPerPage: limit,
+        currentPage: page,
+        totalPages: Math.ceil(totalPage / limit),
+      },
+    };
+  }
+
+  async findOne(id: number) {
+    const product = await this.product.findUnique({ where: { id, available: true } });
+
+    if (!product) {
+      throw new NotFoundException(`Product with id: ${id} not found`);
+    }
+
+    return product;
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+
+    try {
+
+      const {id: __, ...data} = updateProductDto;
+
+      return await this.product.update({
+        where: { id },
+        data: data,
+      });
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === 'P2025'
+      ) {
+        throw new NotFoundException(`Product with id ${id} not found`);
+      }
+
+      throw error;
+    }
+  }
+
+  async remove(id: number) {
+    await this.findOne(id);
+
+    const product = await this.product.update({
+      where: { id },
+      data: {
+        available: false
+      }
+    });
+
+    return product;
+
+    // return this.product.delete({
+    //   where: { id },
+    // });
+  }
+}
